2aa0bb0fa7b79094e80e9b587f2443b5fa89ce0c
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-01-31 00:21:22 +0900

    ExternalControllerに現在操作可能な項目をすべて実装

--- EVMC4U/ExternalController.cs
+++ EVMC4U/ExternalController.cs
@@ -41,7 +41,7 @@ namespace EVMC4U
     public class ExternalController : MonoBehaviour
     {
         [Header("ExternalController v1.1")]
-        public bool enable = false;
+        public bool enable = true;
         [Header("Frame Period")]
         public int PeriodOfStatus = 1;
         public int PeriodOfRoot = 1;
@@ -74,6 +74,7 @@ namespace EVMC4U
 
         [Header("Eye Tracking Target Position")]
         public Transform EyeTrackingTargetTransform = null;
+        public bool EyeTrackingTargetUse = false;
         public bool EyeTrackingTargetEnable = false;
 
         [Header("Response String")]
@@ -92,5 +93,107 @@ namespace EVMC4U
         [Header("Request Information")]
         public bool RequestInformation = false;
 
+        uOSC.uOscClient client = null;
+
+        void Start()
+        {
+            client = GetComponent<uOSC.uOscClient>();
+        }
+        void Update()
+        {
+            if (client == null) {
+                return;
+            }
+            if (!enable) {
+                return;
+            }
+
+            if (PeriodEnable) {
+                client.Send("/VMC/Ext/Set/Period", PeriodOfStatus, PeriodOfRoot, PeriodOfBone, PeriodOfBlendShape, PeriodOfCamera, PeriodOfDevices);
+            }
+            if (DeviceEnable) {
+                string name = null;
+                switch (DeviceMode) {
+                    case VirtualDevice.HMD:
+                        name = "/VMC/Ext/Hmd/Pos";
+                        break;
+                    case VirtualDevice.Controller:
+                        name = "/VMC/Ext/Con/Pos";
+                        break;
+                    case VirtualDevice.Tracker:
+                        name = "/VMC/Ext/Tra/Pos";
+                        break;
+                    default:
+                        name = null;
+                        break;
+                }
+                if (name != null && DeviceTransform != null && DeviceSerial != null) {
+                    client.Send(name,
+                        (string)DeviceSerial,
+                        (float)DeviceTransform.position.x,
+                        (float)DeviceTransform.position.y,
+                        (float)DeviceTransform.position.z,
+                        (float)DeviceTransform.rotation.x,
+                        (float)DeviceTransform.rotation.y,
+                        (float)DeviceTransform.rotation.z,
+                        (float)DeviceTransform.rotation.w);
+                }
+            }
+            if (MidiEnable)
+            {
+                client.Send("/VMC/Ext/Midi/CC/Val", MidiKnob, MidiValue);
+            }
+            if (CameraEnable)
+            {
+                client.Send("/VMC/Ext/Cam",
+                    "Camera",
+                    (float)CameraTransform.position.x,
+                    (float)CameraTransform.position.y,
+                    (float)CameraTransform.position.z,
+                    (float)CameraTransform.rotation.x,
+                    (float)CameraTransform.rotation.y,
+                    (float)CameraTransform.rotation.z,
+                    (float)CameraTransform.rotation.w,
+                    (float)CameraFOV);
+            }
+            if (BlendShapeEnable)
+            {
+                client.Send("/VMC/Ext/Blend/Val", BlendShapeName, BlendShapeValue);
+                client.Send("/VMC/Ext/Blend/Apply");
+            }
+            if (EyeTrackingTargetEnable)
+            {
+                client.Send("/VMC/Ext/Set/Eye",
+                    EyeTrackingTargetUse?(int)1: (int)0,
+                    (float)EyeTrackingTargetTransform.position.x,
+                    (float)EyeTrackingTargetTransform.position.y,
+                    (float)EyeTrackingTargetTransform.position.z);
+            }
+            if (ResponseStringEnable)
+            {
+                client.Send("/VMC/Ext/Set/Res", ResponseString);
+            }
+            if (CalibrationReady)
+            {
+                CalibrationReady = false;
+                client.Send("/VMC/Ext/Set/Calib/Ready");
+            }
+            if (CalibrationExecute)
+            {
+                CalibrationExecute = false;
+                client.Send("/VMC/Ext/Set/Calib/Exec", (int)calibrationMode);
+            }
+            if (ConfigLoad)
+            {
+                ConfigLoad = false;
+                client.Send("/VMC/Ext/Set/Config", ConfigPath);
+            }
+            if (RequestInformation)
+            {
+                //毎フレーム送信要求をする
+                client.Send("/VMC/Ext/Set/Req");
+            }
+        }
     }
+
 }
